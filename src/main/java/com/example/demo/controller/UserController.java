1aed3b13e7aab984baa1f74f4f6f18bca153266e
moMedhat <mmr973320@gmail.com>
2024-10-03 19:15:27 +0300

    [setup]: init

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.UserModel;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/v1")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/getAll")
+    public List<UserModel> getAll(){
+        return this.userService.getAll();
+    }
+}
---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,7 @@
+package com.example.demo.dto;
+
+public class UserDto {
+    public String name;
+    public String email;
+    public String password;
+}

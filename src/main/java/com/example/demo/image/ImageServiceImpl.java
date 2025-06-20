5b19a77fa6b9d2cd9342ee648e9c4a62d30247e1
mohamedmedhat <mmr973320@gmail.com>
2025-06-20 21:37:04 +0300

    (fix): solve mapstruct problem

--- src/main/java/com/example/demo/image/ImageServiceImpl.java
+++ src/main/java/com/example/demo/image/ImageServiceImpl.java
@@ -3,12 +3,14 @@ package com.example.demo.image;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.image.dto.ImageResponseDto;
 import com.example.demo.image.exception.ImageNotFoundException;
 
+import java.io.File;
 import java.io.IOException;
 
 @RequiredArgsConstructor
@@ -17,18 +19,35 @@ public class ImageServiceImpl implements ImageService {
     private final ImageRepository imageRepository;
     private final ImageMapper imageMapper;
 
+    @Value("${IMAGE_UPLOAD_DIR}")
+    private String uploadDir;
+
+    public Image toEntity(MultipartFile file, String uploadDir) throws IOException {
+        File directory = new File(uploadDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        String filePath = uploadDir + file.getOriginalFilename();
+        file.transferTo(new File(filePath));
+        Image image = new Image();
+        image.setFilename(file.getOriginalFilename());
+        image.setFiletype(file.getContentType());
+        image.setFilePath("/images/" + file.getOriginalFilename());
+        return image;
+    }
+
     @Override
     public Mono<ImageResponseDto> saveImage(MultipartFile file) throws IOException {
-        Image image = this.imageMapper.toEntity(file);
-        Mono<Image> savedImage = this.imageRepository.save(image);
-        return this.imageMapper.toResponseDto(savedImage);
+        Image image = this.toEntity(file, uploadDir);
+        return this.imageRepository.save(image)
+                .map(this.imageMapper::toResponseDto);
     }
 
     @Override
     public Mono<ImageResponseDto> findImageById(String id) {
-        Mono<Image> image = this.imageRepository.findById(id)
-                .switchIfEmpty(Mono.error(new ImageNotFoundException(id)));
-        return this.imageMapper.toResponseDto(image);
+        return this.imageRepository.findById(id)
+                .switchIfEmpty(Mono.error(new ImageNotFoundException(id)))
+                .map(this.imageMapper::toResponseDto);
     }
 
     @Override
--- src/main/java/com/example/demo/messages/MessageServiceImpl.java
+++ src/main/java/com/example/demo/messages/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.meeting.dto.request.CreateMessageRequestDto;
 import com.example.demo.messages.dto.response.MessageResponseDto;
+import com.example.demo.messages.exception.MessageNotFoundException;
 import com.example.demo.user.User;
 
 import lombok.RequiredArgsConstructor;
@@ -13,29 +14,28 @@ import reactor.core.publisher.Mono;
 @Service
 public class MessageServiceImpl implements MessageService {
     private final MessageRepository messageRepository;
+    private final MessageMapper messageMapper;
 
     @Override
     public Mono<MessageResponseDto> sendMessage(CreateMessageRequestDto messageData, User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendMessage'");
+        Message message = this.messageMapper.toEntity(messageData, user);
+        return this.messageRepository.save(message)
+                .map(this.messageMapper::toDto);
+
     }
 
     @Override
     public Mono<Boolean> deleteMessage(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteMessage'");
+        this.findMessageById(id);
+        return this.messageRepository.deleteById(id)
+                .thenReturn(true)
+                .onErrorResume(e -> Mono.just(false));
     }
 
-    @Override
-    public Mono<String> handleAsking() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleAsking'");
-    }
+    public Mono<MessageResponseDto> findMessageById(String id) {
+        return this.messageRepository.findById(id)
+                .map(this.messageMapper::toDto)
+                .switchIfEmpty(Mono.error(new MessageNotFoundException(id)));
 
-    @Override
-    public Mono<Void> handleAnswering() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleAnswering'");
     }
-
 }
